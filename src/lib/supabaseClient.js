d0ce56b7c9f725af6113294d843c924eae715b66
andriismail78 <50197753+andriismail78@users.noreply.github.com>
2026-02-08 00:12:00 +0700

    Delete src directory

--- src/lib/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// src/lib/supabaseClient.js
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://frzwffzopzyojgcppvpe.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyendmZnpvcHp5b2pnY3BwdnBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MTM1MTcsImV4cCI6MjA4NTM4OTUxN30.-Up0AI3FRHJvOnjpxqu0i6fekx1uaqNLfH_YsrJKVUU";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
